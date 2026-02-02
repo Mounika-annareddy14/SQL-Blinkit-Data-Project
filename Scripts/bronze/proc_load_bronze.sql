bef1dc78984da7557beab2e9dd4bd864d96fb369
Mounika Annareddy <160157282+Mounika-annareddy14@users.noreply.github.com>
2026-02-02 21:07:28 +0530

    Refactor load_bronze procedure for clarity and updates

--- Scripts/bronze/proc_load_bronze.sql
+++ Scripts/bronze/proc_load_bronze.sql
@@ -16,194 +16,163 @@ Usage Example:
     EXEC bronze.load_bronze;
 ===============================================================================
 */
-use Blinkit_Dwh
-CREATE OR ALTER PROCEDURE bronze.load_bronze as
-begin
-	declare @start_time Datetime , @end_time Datetime, @batch_start datetime, @batch_end datetime;
+GO
 
-	begin try
-		set @batch_start = GETDATE();
-		print '===============================';
-		print 'loading bronze layer';
-		print '===============================';
+CREATE OR ALTER PROCEDURE  bronze.load_bronze AS
+BEGIN
+	DECLARE @start_time DATETIME , @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
+	BEGIN TRY
+		SET @batch_start_time = GETDATE();
 
-
-		print 'Customers Data';
-		set @start_time = GETDATE();
-		truncate table bronze.customers_info;
-
-		bulk insert bronze.customers_info
-		from 'C:\Data\blinkit_customers.csv'
-		with (
-				format = 'CSV',
-				rowterminator = '0x0a',
-                fieldquote = '"',
-				firstrow =  2,
-				fieldterminator=',',
-				tablock);
-		set @end_time = GETDATE();
-		print 'customers_info loaded in' + cast(datediff(second,@start_time, @end_time) as varchar) + ' sec';
+		PRINT '>>>>>><<<<<<<<<<<<<<';
+		PRINT 'LOADING BRONZE LAYER';
+		PRINT '>>>>>><<<<<<<<<<<<<<';
 
 
-		print 'Orders Data';
-		set @start_time = GETDATE();
-		truncate table bronze.orders_info;
+		SET @start_time = GETDATE();
+		PRINT 'Customers Info';
+		TRUNCATE TABLE bronze.customers_info;
+		BULK INSERT bronze.customers_info
+		FROM 'C:\Users\Mounika Reddy\Downloads\archive (5) 2\blinkit_customers.csv'
+		WITH(
+					FIRSTROW = 2,
+					FIELDTERMINATOR = ',',
+					FIELDQUOTE = '"',  
+					CODEPAGE = '65001',
+					TABLOCK);
+		SET @end_time = GETDATE();
+		PRINT 'Customers info loaded in:'+ CAST(DATEDIFF(SECOND,@start_time,@end_time) AS VARCHAR) + 'secs';
 
-		bulk insert bronze.orders_info
-		from  'C:\Data\blinkit_orders.csv'
-		with(
-				format = 'CSV',
-				rowterminator = '0x0a',
-                fieldquote = '"',
-				firstrow=2,
-				fieldterminator=',',
-				tablock);
-		set @end_time = getDate();
-		print 'Orders_info loaded in' + cast(datediff(second,@start_time,@end_time) as varchar) + 'secs';
+		SET @start_time = GETDATE();
+		PRINT 'Orders Info';
+		TRUNCATE TABLE bronze.orders_info;
+		BULK INSERT bronze.orders_info
+		FROM 'C:\Users\Mounika Reddy\Downloads\archive (5) 2\blinkit_orders.csv'
+		WITH (		FIRSTROW = 2,
+					FIELDTERMINATOR = ',',
+					FIELDQUOTE = '"',  
+					CODEPAGE = '65001',
+					TABLOCK);
+		SET @end_time = GETDATE();
+		PRINT 'Order into loaded in:'+ CAST(DATEDIFF(SECOND,@start_time,@end_time) AS VARCHAR) + 'secs';
 
+		SET @start_time = GETDATE();
+		PRINT 'Order items Info';
+		TRUNCATE TABLE bronze.order_items_info;
+		BULK INSERT bronze.order_items_info
+		FROM 'C:\Users\Mounika Reddy\Downloads\archive (5) 2\blinkit_order_items.csv'
+		WITH(
+					FIRSTROW=2,
+					FIELDTERMINATOR = ',',
+					FIELDQUOTE = '"',  
+					CODEPAGE = '65001',
+					TABLOCK);
+		SET @end_time = GETDATE();
+		PRINT 'Order Items info loaded in:'+ CAST(DATEDIFF(SECOND,@start_time,@end_time) AS VARCHAR) + 'secs';
 
+		SET @start_time = GETDATE();
+		PRINT 'Products Info';
+		TRUNCATE TABLE bronze.products_info;
+		BULK INSERT bronze.products_info
+		FROM 'C:\Users\Mounika Reddy\Downloads\archive (5) 2\blinkit_products.csv'
+		WITH (			
+					FIRSTROW=2,
+					FIELDTERMINATOR = ',',
+					FIELDQUOTE = '"',  
+					CODEPAGE = '65001',
+					TABLOCK);
+		SET @end_time = GETDATE();
+		PRINT 'Products Info loaded in:'+ CAST(DATEDIFF(SECOND,@start_time,@end_time) AS VARCHAR) + 'secs';
 
-		print 'order items data';
-		set @start_time = GetDate();
-		truncate table bronze.order_items_info;
+		SET @start_time = GETDATE();
+		PRINT 'Inventory Info';
+		TRUNCATE TABLE bronze.inventory_info;
+		BULK INSERT bronze.inventory_info
+		FROM 'C:\Users\Mounika Reddy\Downloads\archive (5) 2\blinkit_inventoryNew.csv'
+		WITH (		FIRSTROW=2,
+					FIELDTERMINATOR = ',',
+					FIELDQUOTE = '"',  
+					CODEPAGE = '65001',
+					TABLOCK);
+		SET @end_time = GETDATE();
+		PRINT 'Inventory info loaded in:'+ CAST(DATEDIFF(SECOND,@start_time,@end_time) AS VARCHAR) + 'secs';
 
-		bulk insert bronze.order_items_info
-		from 'C:\Data\blinkit_order_items.csv'
-		with(format = 'CSV',
-				rowterminator = '0x0a',
-                fieldquote = '"',
-				firstrow=2,
-				fieldterminator=',',
-				tablock);
-		set @end_time = getDate();
-		print 'Orders_info loaded in' + cast(datediff(second,@start_time,@end_time) as varchar) + 'secs';
+		SET @start_time = GETDATE();
+		PRINT 'Delivery performance Info'; 
+		TRUNCATE TABLE bronze.delivery_performance_info;
+		BULK INSERT bronze.delivery_performance_info
+		FROM 'C:\Users\Mounika Reddy\Downloads\archive (5) 2\blinkit_delivery_performance.csv'
+		WITH(	
+					FIRSTROW=2,
+					FIELDTERMINATOR = ',',
+					FIELDQUOTE = '"',  
+					CODEPAGE = '65001',
+					TABLOCK);
+		SET @end_time = GETDATE();
+		PRINT 'Delivery performance info loaded in:' +  CAST(DATEDIFF(SECOND,@start_time,@end_time) AS VARCHAR) + 'secs';
 
+		SET @start_time = GETDATE();
+		PRINT 'Marketing Performance Info';
+		TRUNCATE TABLE bronze.marketing_performance_info;
+		BULK INSERT bronze.marketing_performance_info
+		FROM 'C:\Users\Mounika Reddy\Downloads\archive (5) 2\blinkit_marketing_performance.csv'
+		WITH(		FIRSTROW=2,
+					FIELDTERMINATOR = ',',
+					FIELDQUOTE = '"',  
+					CODEPAGE = '65001',
+					TABLOCK);
+		SET @end_time = GETDATE();
+		PRINT 'Marketing performance loaded in:'+ CAST(DATEDIFF(SECOND,@start_time,@end_time) AS VARCHAR) + 'secs';
 
-		print 'products data';
-		set @start_time = GetDate();
-		truncate table bronze.products_info;
+		SET @start_time = GETDATE();
+		PRINT 'Customer Feedback Info';
+		TRUNCATE TABLE bronze.customer_feedback_info;
+		BULK INSERT bronze.customer_feedback_info
+		FROM 'C:\Users\Mounika Reddy\Downloads\archive (5) 2\blinkit_customer_feedback.csv'
+		WITH(       FIRSTROW=2,
+					FIELDTERMINATOR = ',',
+					FIELDQUOTE = '"',  
+					CODEPAGE = '65001',
+					TABLOCK);
+		SET @end_time = GETDATE();
+		PRINT 'customer-feedback info loaded in:'+ CAST(DATEDIFF(SECOND,@start_time,@end_time) AS VARCHAR) + 'secs';
 
-		bulk insert bronze.products_info
-		from 'C:\Data\blinkit_products.csv'
-		with(format = 'CSV',
-				rowterminator = '0x0a',
-                fieldquote = '"',
-				firstrow=2,
-				fieldterminator=',',
-				tablock);
-		set @end_time = getDate();
-		print ' loaded in' + cast(datediff(second,@start_time,@end_time) as varchar) + 'secs';
+		SET @start_time = GETDATE();
+		PRINT 'Category Icons info';
+		TRUNCATE TABLE bronze.category_icons_info;
+		BULK INSERT bronze.category_icons_info
+		FROM 'C:\Users\Mounika Reddy\Downloads\archive (5) 2\Category_Icons.csv'
+		WITH (		FIRSTROW=2,
+					FIELDTERMINATOR = ',',
+					FIELDQUOTE = '"',  
+					CODEPAGE = '65001',
+					TABLOCK);
+		SET @end_time = GETDATE();
+		PRINT 'Category icons info loaded in:'+ CAST(DATEDIFF(SECOND,@start_time,@end_time) AS VARCHAR) + 'secs';
 
+		SET @start_time = GETDATE();
+		PRINT 'Rating Icons Info';
+		TRUNCATE TABLE bronze.rating_icons_info;
+		BULK INSERT bronze.rating_icons_info
+		FROM 'C:\Users\Mounika Reddy\Downloads\archive (5) 2\Rating_Icon.csv' 
+		WITH(       FIRSTROW=2,
+					FIELDTERMINATOR = ',',
+					FIELDQUOTE = '"',  
+					CODEPAGE = '65001',
+					TABLOCK);
+		SET @end_time = GETDATE();
+		PRINT 'Rating icons Info loaded in:'+ CAST(DATEDIFF(SECOND,@start_time,@end_time) AS VARCHAR) + 'secs';
 
-		print 'inventory data';
-		set @start_time = GetDate();
-		truncate table bronze.inventory_info;
-
-		bulk insert bronze.inventory_info
-		from 'C:\Data\blinkit_inventoryNew.csv'
-		with(format = 'CSV',
-				rowterminator = '0x0a',
-                fieldquote = '"',
-				firstrow=2,
-				fieldterminator=',',
-				tablock);
-		set @end_time = getDate();
-		print ' loaded in' + cast(datediff(second,@start_time,@end_time) as varchar) + 'secs';
-
-
-		print 'delivery performance data';
-		set @start_time = GetDate();
-		truncate table bronze.delivery_performance_info;
-
-		bulk insert bronze.delivery_performance_info
-		from 'C:\Data\blinkit_delivery_performance.csv'
-		with(format = 'CSV',
-				rowterminator = '0x0a',
-                fieldquote = '"',
-				firstrow=2,
-				fieldterminator=',',
-				tablock);
-		set @end_time = getDate();
-		print ' loaded in' + cast(datediff(second,@start_time,@end_time) as varchar) + 'secs';
-
-
-
-		print 'marketing performance data';
-		set @start_time = GetDate();
-		truncate table bronze.marketing_performance_info;
-
-		bulk insert bronze.marketing_performance_info
-		from 'C:\Data\blinkit_marketing_performance.csv'
-		with(format = 'CSV',
-				rowterminator = '0x0a',
-                fieldquote = '"',
-				firstrow=2,
-				fieldterminator=',',
-				tablock);
-		set @end_time = getDate();
-		print ' loaded in' + cast(datediff(second,@start_time,@end_time) as varchar) + 'secs';
-
-
-		print 'CUSTOMER FEEDBACK data';
-		set @start_time = GetDate();
-		truncate table bronze.customer_feedback_info;
-
-		bulk insert bronze.customer_feedback_info
-		from 'C:\Data\blinkit_customer_feedback.csv'
-		with(format = 'CSV',
-				rowterminator = '0x0a',
-                fieldquote = '"',
-				firstrow=2,
-				fieldterminator=',',
-				tablock);
-		set @end_time = getDate();
-		print ' loaded in' + cast(datediff(second,@start_time,@end_time) as varchar) + 'secs';
-
-
-
-		print 'category icons data';
-		set @start_time = GetDate();
-		truncate table bronze.category_icons_info;
-
-		bulk insert bronze.category_icons_info
-		from 'C:\Data\blinkit_Category_Icons.csv'
-		with(format = 'CSV',
-				rowterminator = '0x0a',
-                fieldquote = '"',
-				firstrow=2,
-				fieldterminator=',',
-				tablock);
-		set @end_time = getDate();
-		print ' loaded in' + cast(datediff(second,@start_time,@end_time) as varchar) + 'secs';
-
-
-		print 'rating icons data';
-		set @start_time = GetDate();
-		truncate table bronze.rating_icons_info;
-
-		bulk insert bronze.rating_icons_info
-		from 'C:\Data\blinkit_Rating_Icon.csv'
-		with(format = 'CSV',
-				rowterminator = '0x0a',
-                fieldquote = '"',
-				firstrow=2,
-				fieldterminator=',',
-				tablock);
-		set @end_time = getDate();
-		print ' loaded in' + cast(datediff(second,@start_time,@end_time) as varchar) + 'secs';
-
-	end try
-
-	Begin catch
-			print 'Error in Bronze';
-			print error_message();
-	end catch
-end;
-
-
-select * from bronze.customers_info;
-
-
+		SET @batch_end_time = GETDATE();
+		PRINT 'Loading Bronze layer is Completed';
+		PRINT 'Total Duration is:' + CAST(DATEDIFF(second,@batch_start_time,@batch_end_time) AS VARCHAR) +'secs';
+	END TRY
+	BEGIN CATCH
+	PRINT 'ERROR Occured during loading Bronze layer';
+	PRINT 'Error message' + CAST(ERROR_NUMBER() AS VARCHAR);
+	PRINT 'Error message' + CAST(ERROR_STATE() AS VARCHAR);
+	END CATCH
+END
 
 
 
